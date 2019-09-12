fea07acf6b14147fa9ecd187552545e9c1c19b56
Jerma066 <Jerma066@users.noreply.github.com>
2019-09-12 14:38:04 +0300

    Обновление учебных материалов. Задача GeneralMechanisms: иницилизация количества итераций при запуске.

--- GeneralMechanisms/GeneralMechanisms.cpp
+++ GeneralMechanisms/GeneralMechanisms.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <omp.h>
+#include <cstdlib>
 
 using namespace std;
 
-int main(){
+int main(int argc, char* argv[]){
 	int i;
 	long int sum = 0;
-	int N = 10000;
+	int N = atoi(argv[1]);
 	
 	#pragma omp parallel for if(N>4) private(i) reduction(+:sum) shared(N) schedule(static) 
 		for(i = 0; i < N+1; i++)
